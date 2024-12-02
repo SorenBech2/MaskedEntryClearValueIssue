5a6e22c14a238863e1dcbedd4a3d1cf1a8137cae
Søren Bech <soren-bech@outlook.com>
2024-12-02 13:23:29 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MaskedEntryClearValueIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearForm_Clicked(object sender, EventArgs e)
+        {
+            Entry1.Value = string.Empty;
+            Entry2.Value = string.Empty;
+        }
+    }
+
+}
